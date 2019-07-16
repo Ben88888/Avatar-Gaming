3827bfa53ce059f68f5e688becff7e1b718d0cbd
Ben <benpilot1010@gmail.com>
2019-07-16 18:45:15 +0300

    updated calculator (fixed game flaw and added score)

--- js/calculator.js
+++ js/calculator.js
@@ -35,3 +35,30 @@ const calculate = (num1, num2, operation) => {
         return calculate(num1, num2, randomOperation);
     }
 }
+const randomNumbers = () => {
+    DoYouWantToContinue = "yes";
+    alert("Welcome to the calculator minigame!");
+    let score = 0;
+    while(DoYouWantToContinue == 'Yes' || DoYouWantToContinue == 'yes' || DoYouWantToContinue == 'y') {
+        const num1 = Math.floor(Math.random() * 10000);
+        const num2 = Math.floor(Math.random() * 10000);
+        document.getElementById('firstNumber').value = num1;
+        document.getElementById('secondNumber').value = num2;
+        const randomSolution = calculate(num1, num2, '?');
+        alert("Num1 = " + num1 + " Num 2 = " + num2 + " solution = " + randomSolution);
+        const guessOperation = prompt("Please guess the operation!");
+        const guessSolution = calculate(num1, num2, guessOperation);
+        if (randomSolution == guessSolution) {
+            score++;
+            alert('Correct and your score is ' + score);
+        } else {
+            score--;
+            alert('Wrong and your score is ' + score);
+        }
+        DoYouWantToContinue = prompt('Do You Want To Continue?');
+    }
+    alert('Thanks for playing! your total score is ' + score)
+    return randomSolution;
+    
+}
+
